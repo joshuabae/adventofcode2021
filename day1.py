a24b204d5f1e75a725f82bebc9c2321c5b96e4bf
Joshua Bae <joshua.bae@credera.com>
2021-12-01 10:49:11 -0600

    solved part 1

---
+++ day1.py
@@ -0,0 +1,20 @@
+### part1
+
+# read txt file
+with open('input.txt') as f:
+    data = f.read().splitlines() 
+    num_list = [int(num) for num in data]
+
+print(num_list[0:10])
+
+def count_inc(depth_lst):
+    count = 0 
+    for j in range(1, len(depth_lst) - 1):
+        if depth_lst[j] > depth_lst[j-1]:
+            count += 1
+    return count
+
+result = count_inc(num_list)
+print(result)
+
+### part2
