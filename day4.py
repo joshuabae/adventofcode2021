6106ecb4de05023381e7c6fb4e478e3c3a8ea3a1
Joshua Bae <joshua.bae@credera.com>
2021-12-05 10:03:41 -0600

    made progress on day4

--- day4.py
+++ day4.py
@@ -7,12 +7,24 @@ with open('files/day4.txt') as f:
 
     boards = []
     for i in range(1, len(data)-1, 6):
-        board = [[str(num) for num in line.split()] for line in data[i:i+5]]
+        board = [[[str(num), False] for num in line.split()] for line in data[i:i+5]]
         boards.append(board)
 
+print(boards[1])
 
-def tic_tac(numbers, game_boards):
-    return None
+def mark_rows(number, game_board):
+    for row in game_board:
+        for col in row:
+            if col[0] == number:
+                col[1] = True
+    return game_board
+
+def check_row(game_board):
+    for 
 
 
+def check_col(game_board):
     
+
+
+print(mark_rows('48', boards[1]))
